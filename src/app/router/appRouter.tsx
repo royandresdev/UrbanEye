8015f1133bfa99883e5470dc0a97f15301c5bf7b
royandresdev <royhuamanavila@gmail.com>
2026-03-04 20:28:50 -0500

    ✨ Agregar autenticación a rutas 🚀 - Se implementa la función `requireAuth` para redirigir a la página de inicio de sesión si no hay sesión activa, mejorando la seguridad de las rutas 🎉🔒

--- src/app/router/appRouter.tsx
+++ src/app/router/appRouter.tsx
@@ -1,13 +1,28 @@
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, redirect } from 'react-router-dom'
 import { LoginPage } from '../../features/auth/LoginPage'
 import { RegisterPage } from '../../features/auth/RegisterPage'
 import { HomePage } from '../../features/home/HomePage'
 import { NewReportPage } from '../../features/reports/NewReportPage'
 import { ReportsOverviewPage } from '../../features/reports/ReportsOverviewPage'
+import { supabase } from '../../shared/lib/supabase'
+
+async function requireAuth() {
+  const {
+    data: { session },
+    error,
+  } = await supabase.auth.getSession()
+
+  if (error || !session) {
+    throw redirect('/auth')
+  }
+
+  return null
+}
 
 export const appRouter = createBrowserRouter([
   {
     path: '/',
+    loader: requireAuth,
     element: <HomePage />,
   },
   {
@@ -20,10 +35,12 @@ export const appRouter = createBrowserRouter([
   },
   {
     path: '/reports/new',
+    loader: requireAuth,
     element: <NewReportPage />,
   },
   {
     path: '/reports',
+    loader: requireAuth,
     element: <ReportsOverviewPage />,
   },
 ])
